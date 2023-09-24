e1e72aabbce60f6ff775b2d9f25c88646156f3b8
jayellezo0801 <144531813+jayellezo0801@users.noreply.github.com>
2023-09-24 22:34:24 +0800

    Add files via upload

---
+++ CJEPortfolio/script.js
@@ -0,0 +1,42 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Student", "Musician", "Vocalist"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+var typed = new Typed(".single-text", {
+    strings: ["USTP JASAAN"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+
+var typed = new Typed(".education-text", {
+    strings: ["Educational Record"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+var typed = new Typed(".hobbies-text", {
+    strings: ["Hobbies"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+var typed = new Typed(".sport-text", {
+    strings: ["Playing billiard"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+
